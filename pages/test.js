3bb52cd1eeab91a823e024d6719ebf798b2e642a
MochamadRicoPratamaPutra <mricopratamaputra@gmail.com>
2021-09-05 21:57:34 +0700

    adding add product, responsive

--- pages/test.js
+++ pages/test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from '../components/layout';
 const Test = () => {
+  console.log(process.env.NEXT_PUBLIC_BASE_URL)
   return (
     <div>
       <Layout isAuth={false} active="home" login={true}>
