e7b01a79895d8bd8693c335882c916cb43e6007e
MochamadRicoPratamaPutra <mricopratamaputra@gmail.com>
2021-09-08 00:30:39 +0700

    adding conditional rendering for user and non user

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,13 +1,15 @@
 import NavBar from './navbar';
 import Head from 'next/head';
 import Footer from './footer';
+import { useSelector } from 'react-redux';
 const Layout = (props) => {
+  const {profile} = useSelector(state => state.user)
   return (
     <div>
       <Head>
         <title>{props.title}</title>
       </Head>
-      <NavBar isAuth={props.isAuth} active={props.active} />
+      <NavBar isAuth={Object.keys(profile).length === 0 ? false : true} active={props.active} />
       <div className="layoutBox">{props.children}</div>
       <Footer login={props.login} />
     </div>
